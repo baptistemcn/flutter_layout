a5183b827643a5be2590dac8e1cf2fd54caa5070
Baptiste Marcon <baptistemcn@icloud.com>
2021-08-22 18:05:21 +0200

    feat: button row

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,15 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
@@ -46,9 +55,30 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Demo Layout'),
         ),
         body: Column(
-          children: [titleSection],
+          children: [titleSection, buttonSection],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
